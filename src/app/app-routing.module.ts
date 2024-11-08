6210fd682015686c357fad799430141bbcc776b6
WillianFarias <williansousafarias@gmail.com>
2024-11-08 03:08:29 -0300

    criacao de modulo cursos e utilizacao de lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //caso seja vazio ele deve redirecionar para courses
+  { path: '', pathMatch: 'full', redirectTo: 'courses'},
+  //este modulo so sera carregado quando for acessado o path courses
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
