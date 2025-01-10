f420d5a85800270062a5a5797d6401917ee06134
Willian Farias <williansousafarias@gmail.com>
2025-01-10 01:11:50 -0300

    criacao do campo nome no form de cadastro de curso

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.css']
+  styleUrls: ['./course-form.component.css'],
 })
 export class CourseFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      name: [null],
+      category: [null]
+    });
   }
 
+  ngOnInit(): void {}
 }
